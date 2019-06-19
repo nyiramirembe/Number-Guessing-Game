407e426cb5d88dd7ffa01e2abd4c56b8cde5b665
nyiramirembe <51477988+nyiramirembe@users.noreply.github.com>
2019-06-19 01:34:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var ranNum =Math.floor(Math.random()*100)+1;//this the fucntion that enable us to get the random number
+for (ranNum=1; ranNum<100; ranNum++)//ranNum is the random number that the computer will select
+
+
+for(i=1; i<10; i++ )
+{
+var evenOdd=ranNum%2;//here we want to get the modulus of the random number selected pwith 2
+if (evenOdd=0);
+console.log("Number is even")
+else 
+	console.log("number is odd")
+}
+
+Let prompt()
